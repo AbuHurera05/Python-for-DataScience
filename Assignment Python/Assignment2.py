6dee02ab8df3f052184b1ebeb5cd746f6cd04b34
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-03-22 22:18:58 +0500

    Update

---
+++ Assignment Python/Assignment2.py
@@ -0,0 +1,34 @@
+#Qno1:Print the First and Last Character of a String. 
+a='Pakistan'
+# print('First: ',a[0], ' Last: ',a[len(a)-1])
+
+#Qno2: Extract and Print the Second and Second-Last Character.  
+# print('Second: ',a[1], 'Second Last: ',a[len(a)-2])
+
+#Qno3: Print Each Character of a String Using a Loop.
+# for i in a:
+#     print(i,end=' ')
+
+#Qno4:  Reverse a String Using Indexing. 
+# print(a[::-1])
+
+# Qno5:Extract a Substring from Index 2 to 5.
+# print(a[2:5])
+
+#Qno6: Check if a Character Exists at a Specific Index.
+# index=int(input("Enter indext to check specific character: "))
+# print(a[index])
+
+#Qno7:Print a String in Reverse Order Using a Loop.
+# for i in range(len(a)):
+#     print(a[len(a)-1-i],end=' ')
+
+#Qno8: Replace a Character at a Specific Index with a New Character.
+a.replace('Pak')  
+print(a)
+
+# Qno9:  Extract Only Even-Indexed Characters from a String.  
+# print(a[0::2])
+
+# Qno10:  Extract Only Odd-Indexed Characters from a String.
+# print(a[1::2])
