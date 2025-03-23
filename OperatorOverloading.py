5e4338850929572b176e3f7e732c111738e981fa
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-03-23 22:57:20 +0500

    Update

--- OperatorOverloading.py
+++ OperatorOverloading.py
@@ -10,7 +10,23 @@ class Student:
 
         return m3
     
+    def __gt__(self,other):
+        r1=self.m1+self.m2
+        r2=other.m1+other.m2
+        if r1>r2:
+            return True
+        else:
+            return False
+        
+    def __str__(self):
+        return '{} {} '.format (self.m1,self.m2)
+    
+    
 s1=Student(44,55)
 s2=Student(88,33)
 s3=s1+s2
 print(s3.m2)
+if s1>s2:
+    print(s1,' Wins')
+else:
+    print(s2,' Wins')
